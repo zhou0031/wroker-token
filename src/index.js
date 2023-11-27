69f3e365e9338983f12e4c0dd1f625fed98faf4b
ryan <ryan.z.williams@gmail.com>
2023-11-27 12:08:35 +0800

    auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import jwt from '@tsndr/cloudflare-worker-jwt'
+import {error,json,Router} from 'itty-router'
+
+const router = Router()
+
+async function auth(request,env,ctx){
+	const authResponse = await env.auth.fetch(request.clone())
+	if (authResponse.status !== 200) 
+		return error(401,"Invalid Request")	
+}
+
+router
+	.all("*",auth)
 
 export default {
+	
+	async fetch(request, env, ctx) {
+		return router.handle(request,env,ctx).then(json).catch(error)
+	},
 
 	async scheduled(event, env, ctx) {
 		const payload={
